cd54d8ed87c4d32e48551a2e7994394fb2185a32
marcelloberetta <marcelloberetta4@gmail.com>
2021-06-14 01:29:21 +0200

    Update

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+
+
+const app = new Vue({
+
+  el: '#app',
+  data:{
+
+    albums:[],
+    genres:[],
+    genreToSearch: 'all',
+    apiURL: 'http://localhost:8888/dischi-php-ajax/api.php'
+
+  },
+  methods:{
+
+    getAPI(){
+      axios.get(this.apiURL,{
+        params:{
+          genre: this.genreToSearch
+        }
+      })
+      .then(res => {
+
+        this.albums = res.data.albums;
+        this.genres = res.data.genres;
+        console.log(this.genres);
+
+      }).catch(err => {
+
+        console.log(err);
+
+      })
+    }
+
+  },
+  created(){
+    this.getAPI();
+  }
+
+});
